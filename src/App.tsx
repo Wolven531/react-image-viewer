bd1f47787f96a847efa6851d805c8ab3b206aa6a
Anthony Williams <anthony.williams.cs@gmail.com>
2019-02-13 20:51:42 -0700

    Setup basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,74 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+
 import './App.css'
 
-class App extends Component {
+interface IAppState {
+	currentImageIndex: number
+	images: string[]
+	movementFromStart: number
+}
+
+class App extends Component<{}, IAppState> {
+	private readonly imageHeight = 200
+	private readonly imageWidth = 300
+
+	constructor(props: any) {
+		super(props)
+		this.state = {
+			currentImageIndex: 0,
+			images: [
+				'//thispersondoesnotexist.com',
+				'//picsum.photos/200/300?image=0',
+				'//picsum.photos/200/300?image=1',
+				'//picsum.photos/200/300?image=2',
+				'//picsum.photos/200/300?image=3'
+			],
+			movementFromStart: 0
+		}
+	}
+
+	componentDidMount() {
+		window.document.title = 'RIV: React Image Viewer'
+	}
+
 	render() {
+		const { images, movementFromStart } = this.state
+
 		return (
 			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.tsx</code> and save to reload.
-		  </p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-		  </a>
-				</header>
+				<h1>RIV: React Image Viewer</h1>
+				<p>Number of images: {images.length}</p>
+				<article className="main"
+					onWheel={this.handleWheel}
+					style={{ height: `${this.imageHeight}px`, width: `${this.imageWidth}px` }}>
+					<section className="image-swiper" style={{ transform: `translateX(${movementFromStart * -1}px)` }}>
+						{images.map((src: string) =>
+							<img key={src} src={src} height="100%" width="100%" />)}
+					</section>
+				</article>
 			</div>
 		)
+	}
+
+	private handleWheel = (evt: React.WheelEvent<HTMLElement>) => this.scrollTheViewer(evt.deltaX)
+
+	private scrollTheViewer = (deltaHorizontal: number) => {
+		this.setState(state => {
+			const lastPossibleIndex = state.images.length - 1
+			const maximumXScroll = lastPossibleIndex * this.imageWidth
+			const minimumXScroll = 0
+			let updatedXScroll = state.movementFromStart + deltaHorizontal
+
+			if (updatedXScroll < minimumXScroll) {
+				updatedXScroll = minimumXScroll
+			}
+
+			if (updatedXScroll > maximumXScroll) {
+				updatedXScroll = maximumXScroll
+			}
+
+			return { movementFromStart: updatedXScroll }
+		})
 	}
 }
 
